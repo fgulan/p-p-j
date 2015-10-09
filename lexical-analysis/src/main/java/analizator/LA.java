b71c47009838e6dcfa6f00fbad0d64e8ae81b5de
Jan Kelemen <jkeleme@gmail.com>
2015-10-09 23:46:37 +0200

    create eclipse project and the appropriate folder structure

---
+++ lexical-analysis/src/main/java/analizator/LA.java
@@ -0,0 +1,22 @@
+/**
+ * <code>LA</code> class is required by the evaluator, to contain a entry point for the lexical analyzer generator.
+ *
+ * @author Jan Kelemen
+ *
+ * @version 1.0.0
+ */
+public class LA {
+
+    /**
+     * Entry point for lexical analyzer generator program.
+     *
+     * @param args
+     *            command line arguments aren't used.
+     * @since 1.0.0
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
