f643fa57f3f7267f9aa41947d4d3421dd50dbfe0
Jan Kelemen <jkeleme@gmail.com>
2015-10-20 23:01:13 +0200

    implement lexical analyzer generator

--- lexical-analysis/src/main/java/GLA.java
+++ lexical-analysis/src/main/java/GLA.java
@@ -1,3 +1,16 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import hr.fer.zemris.ppj.lexical.analysis.analyzer.AnalyzerDefinition;
+import hr.fer.zemris.ppj.lexical.analysis.analyzer.rules.RuleDefinition;
+import hr.fer.zemris.ppj.lexical.analysis.text.manipulation.RegularExpressionManipulator;
+
 /**
  * <code>GLA</code> class is required by the evaluator, to contain a entry point for the lexical analyzer generator.
  *
@@ -7,6 +20,10 @@
  */
 public class GLA {
 
+    private static final List<String> analyzerStates = new ArrayList<>();
+    private static final List<String> lexemeNames = new ArrayList<>();
+    private static final List<RuleDefinition> rules = new ArrayList<>();
+
     /**
      * Entry point for lexical analyzer generator program.
      *
@@ -15,8 +32,62 @@ public class GLA {
      * @since 1.0.0
      */
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            readInputData(reader);
+        }
+        catch (final IOException e) {
+            System.err.println(e.getMessage());
+        }
 
+        try (OutputStreamWriter writer = new FileWriter(new File("analizator/definition.txt"))) {
+            writeAnalyzerDefinition(writer, new AnalyzerDefinition(analyzerStates, lexemeNames, rules));
+        }
+        catch (final IOException e) {
+            System.err.println(e.getMessage());
+        }
     }
 
+    private static void readInputData(final BufferedReader reader) throws IOException {
+        String line = reader.readLine();
+
+        final List<String> regularDefinitions = new ArrayList<>();
+        while ((line != null) && line.startsWith("{")) {
+            regularDefinitions.add(line);
+            line = reader.readLine();
+        }
+
+        if ((line != null) && line.startsWith("%X")) {
+            for (final String state : line.substring(3).split(" ")) {
+                analyzerStates.add(state);
+            }
+            line = reader.readLine();
+        }
+
+        if ((line != null) && line.startsWith("%L")) {
+            for (final String name : line.substring(3).split(" ")) {
+                lexemeNames.add(name);
+            }
+            line = reader.readLine();
+        }
+
+        final RegularExpressionManipulator manipulator = new RegularExpressionManipulator(regularDefinitions);
+        while (line != null) {
+            final String analyzerState = line.substring(1, line.indexOf('>'));
+            final String regularExpression =
+                    manipulator.removeRegularDefinitions(line.substring(line.indexOf('>') + 1));
+            final List<String> actions = new ArrayList<>();
+            while (!line.startsWith("}")) {
+                actions.add(line);
+                line = reader.readLine();
+            }
+            line = reader.readLine();
+
+            rules.add(new RuleDefinition(analyzerState, regularExpression, actions));
+        }
+    }
+
+    private static void writeAnalyzerDefinition(final OutputStreamWriter writer,
+            final AnalyzerDefinition definition) throws IOException {
+        writer.write(definition.toString());
+    }
 }
---
+++ lexical-analysis/src/main/java/hr/fer/zemris/ppj/lexical/analysis/analyzer/AnalyzerDefinition.java
@@ -0,0 +1,74 @@
+package hr.fer.zemris.ppj.lexical.analysis.analyzer;
+
+import java.util.Collection;
+import java.util.List;
+
+import hr.fer.zemris.ppj.lexical.analysis.analyzer.rules.RuleDefinition;
+
+/**
+ * <code>AnalyzerDefinition</code> contains a definition of the analyzer.
+ *
+ * @author Jan Kelemen
+ *
+ * @version 1.0
+ */
+public class AnalyzerDefinition {
+
+    private final List<String> analyzerStates;
+    private final List<String> lexemeNames;
+    private final List<RuleDefinition> rules;
+
+    /**
+     * Class constructor, creates a analyzer definition from the specified structure.
+     *
+     * @param analyzerStates
+     *            analyzer states.
+     * @param lexemeNames
+     *            lexeme names
+     * @param rules
+     *            analyzer rules
+     * @since 1.0
+     */
+    public AnalyzerDefinition(final List<String> analyzerStates, final List<String> lexemeNames,
+            final List<RuleDefinition> rules) {
+        this.analyzerStates = analyzerStates;
+        this.lexemeNames = lexemeNames;
+        this.rules = rules;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * Format: list of analyzer states. <br>
+     * <br>
+     * list of lexeme names. <br>
+     * <br>
+     * list of rule definitions... <br>
+     *
+     * All lists are space delimited.
+     *
+     * @see hr.fer.zemris.ppj.lexical.analysis.analyzer.rules.RuleDefinition#toString()
+     * @see java.lang.Object#toString()
+     * @since 1.0
+     */
+    @Override
+    public String toString() {
+        String result = "";
+        result += collectionToString(analyzerStates) + "\n";
+        result += "\n";
+        result += collectionToString(lexemeNames) + "\n";
+        result += "\n";
+        for (final RuleDefinition definition : rules) {
+            result += definition.toString() + "\n";
+        }
+        return result;
+    }
+
+    private String collectionToString(final Collection<?> list) {
+        String result = "";
+        for (final Object item : list) {
+            result += item + " ";
+        }
+        return result.substring(0, result.length() - 1);
+    }
+}
