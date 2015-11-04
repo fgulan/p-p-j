bbed5983ad55c77aaa99c89b2e6f8fc00ad503d8
Jan Kelemen <jkeleme@gmail.com>
2015-11-04 21:51:06 +0100

    add classes required by the evaluator

---
+++ syntax-analysis/src/main/java/GSA.java
@@ -0,0 +1,22 @@
+/**
+ * <code>GSA</code> class is required by the evaluator, to contain a entry point for the syntax analyzer generator.
+ *
+ * @author Jan Kelemen
+ *
+ * @version alpha
+ */
+public class GSA {
+
+    /**
+     * Entry point for the syntax analyzer generator.
+     *
+     * @param args
+     *            command line arguments aren't used
+     * @since alpha
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ syntax-analysis/src/main/java/analizator/SA.java
@@ -0,0 +1,22 @@
+/**
+ * <code>SA</code> class is required by the evaluator, to contain a entry point for the syntax analyzer.
+ *
+ * @author Jan Kelemen
+ *
+ * @version alpha
+ */
+public class SA {
+
+    /**
+     * Entry point for the syntax analyzer.
+     *
+     * @param args
+     *            command line arguments aren't used
+     * @since alpha
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
