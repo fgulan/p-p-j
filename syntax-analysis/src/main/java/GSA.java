3031150fe017290078b4c1feef756833c22d9099
Jan Kelemen <jkeleme@gmail.com>
2015-11-07 17:16:01 +0100

    implement reading of grammar from stdin in generator

--- syntax-analysis/src/main/java/GSA.java
+++ syntax-analysis/src/main/java/GSA.java
@@ -1,3 +1,16 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Set;
+
+import hr.fer.zemris.ppj.grammar.Grammar;
+import hr.fer.zemris.ppj.grammar.GrammarBuilder;
+import hr.fer.zemris.ppj.grammar.ProductionParser;
+
 /**
  * <code>GSA</code> class is required by the evaluator, to contain a entry point for the syntax analyzer generator.
  *
@@ -7,6 +20,11 @@
  */
 public class GSA {
 
+    private static final Set<String> nonterminalSymbols = new HashSet<>();
+    private static final Set<String> terminalSymbols = new HashSet<>();
+    private static final Set<String> syncSymbols = new HashSet<>();
+    private static Grammar grammar;
+
     /**
      * Entry point for the syntax analyzer generator.
      *
@@ -15,8 +33,54 @@ public class GSA {
      * @since alpha
      */
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            readInputData(reader);
+        }
+        catch (final IOException e) {
+            System.err.println(e.getMessage());
+        }
 
+        try (OutputStreamWriter writer = new FileWriter(new File("analizator/definition.txt"))) {
+            // writeAnalyzerDefinition(writer, new AnalyzerDefinition(analyzerStates, lexemeNames, rules));
+        }
+        catch (final IOException e) {
+            System.err.println(e.getMessage());
+        }
+
+    }
+
+    private static void readInputData(BufferedReader reader) throws IOException {
+        String originalStartSymbol = null;
+        for (String symbol : reader.readLine().substring(3).split(" ")) {
+            nonterminalSymbols.add(symbol);
+            if (originalStartSymbol == null) {
+                originalStartSymbol = symbol;
+            }
+        }
+        nonterminalSymbols.add("<$>");
+
+        for (String symbol : reader.readLine().substring(3).split(" ")) {
+            terminalSymbols.add(symbol);
+        }
+
+        for (String symbol : reader.readLine().substring(3).split(" ")) {
+            syncSymbols.add(symbol);
+        }
+
+        GrammarBuilder builder = new GrammarBuilder(nonterminalSymbols, terminalSymbols, "<$>");
+        String line = reader.readLine();
+        do {
+            String leftSide = line;
+            do {
+                String rightSide = reader.readLine();
+                line = reader.readLine();
+                builder.addProduction(ProductionParser.fromText(leftSide, rightSide));
+            } while ((line != null) && line.startsWith(" "));
+        } while (line != null);
+
+        builder.addProduction(ProductionParser.fromText("<$> " + originalStartSymbol));
+
+        grammar = builder.build();
     }
 
 }
