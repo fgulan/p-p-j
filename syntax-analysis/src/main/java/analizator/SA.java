e6efdffbc9da68e9e02dc360a61b4c25397dac50
Jan Kelemen <jkeleme@gmail.com>
2015-11-09 23:25:39 +0100

    add skeleton for the syntax analyzer

--- syntax-analysis/src/main/java/analizator/SA.java
+++ syntax-analysis/src/main/java/analizator/SA.java
@@ -1,3 +1,15 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import hr.fer.zemris.ppj.Lexeme;
+import hr.fer.zemris.ppj.UniformSymbol;
+import hr.fer.zemris.ppj.lr1.parser.LR1ParserActionTable;
+
 /**
  * <code>SA</code> class is required by the evaluator, to contain a entry point for the syntax analyzer.
  *
@@ -7,6 +19,9 @@
  */
 public class SA {
 
+    private static final List<Lexeme> lexemes = new ArrayList<>();
+    private static LR1ParserActionTable parserTable;
+
     /**
      * Entry point for the syntax analyzer.
      *
@@ -15,8 +30,42 @@ public class SA {
      * @since alpha
      */
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            readUniformSymbols(reader);
+        }
+        catch (IOException e) {
+            System.err.println("io on reading lexemes");
+            System.exit(0);
+        }
 
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("table.txt")))) {
+            readParserActionTable(reader);
+        }
+        catch (FileNotFoundException e) {
+            System.err.println("no parser table");
+            System.exit(0);
+        }
+        catch (IOException e) {
+            System.err.println("io on reading parser table");
+            System.exit(0);
+        }
     }
 
+    /*
+     * Reads uniform symbols and parses them to lexemes.
+     */
+    private static void readUniformSymbols(BufferedReader reader) throws IOException {
+        String uniformSymbol = reader.readLine();
+        do {
+            lexemes.addAll(new UniformSymbol(uniformSymbol).toLexemes());
+            uniformSymbol = reader.readLine();
+        } while (uniformSymbol != null);
+    }
+
+    /*
+     * Reads the LR(1) parser action table.
+     */
+    private static void readParserActionTable(BufferedReader reader) throws IOException {
+        // TODO: Wait for Domagoj
+    }
 }
--- syntax-analysis/src/main/java/hr/fer/zemris/ppj/lr1/parser/LR1Parser.java
+++ syntax-analysis/src/main/java/hr/fer/zemris/ppj/lr1/parser/LR1Parser.java
@@ -1,12 +1,43 @@
 package hr.fer.zemris.ppj.lr1.parser;
 
+import java.io.PrintStream;
+import java.util.List;
+
+import hr.fer.zemris.ppj.Lexeme;
 
 /**
- * <code>LR1Parser</code> 
- * @author Jan Kelemen
+ * <code>LR1Parser</code> represents a canonical LR(1) parser.
  *
- * @version 
+ * @author Matea Sabolic
+ *
+ * @version alpha
  */
 public class LR1Parser {
+
+    /**
+     * Class constructor, specifies the actions of the parser.
+     *
+     * @param table
+     *            the table of actions.
+     * @since alpha
+     */
+    public LR1Parser(final LR1ParserActionTable table) {
+        // TODO: needs to be implemented. @Matea
+    }
+
+    /**
+     * Analyzes the list of lexemes.
+     *
+     * @param lexemes
+     *            the lexemes.
+     * @param outputStream
+     *            the output stream.
+     * @param errorStream
+     *            the error output stream.
+     * @since alpha
+     */
+    public void analyze(List<Lexeme> lexemes, PrintStream outputStream, PrintStream errorStream) {
+        // TODO: needs to be implemented. @Matea
+    }
 
 }
