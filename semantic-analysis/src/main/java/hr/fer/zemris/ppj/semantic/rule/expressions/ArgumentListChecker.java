4066c249782fb048a785d51f2161bd7eed397f79
Jan Kelemen <jan.kelemen@fer.hr>
2015-12-12 19:36:36 +0100

    add more checkers

--- semantic-analysis/src/main/java/hr/fer/zemris/ppj/semantic/rule/expressions/ArgumentListChecker.java
+++ semantic-analysis/src/main/java/hr/fer/zemris/ppj/semantic/rule/expressions/ArgumentListChecker.java
@@ -1,6 +1,13 @@
 package hr.fer.zemris.ppj.semantic.rule.expressions;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import hr.fer.zemris.ppj.Attribute;
 import hr.fer.zemris.ppj.Node;
+import hr.fer.zemris.ppj.SemanticErrorReporter;
+import hr.fer.zemris.ppj.VariableType;
 import hr.fer.zemris.ppj.semantic.rule.Checker;
 
 /**
@@ -34,7 +41,47 @@ public class ArgumentListChecker implements Checker {
      */
     @Override
     public boolean check(Node node) {
-        // TODO Auto-generated method stub
+        Node firstChild = node.getChild(0);
+        String firstSymbol = node.name();
+
+        // <lista_argumenata> ::= <izraz_pridruzivanja>
+        if ("<lista_argumenata>".equals(firstSymbol)) {
+
+            // 1. provjeri(<izraz_pridruzivanja)
+            if (!firstChild.check()) {
+                SemanticErrorReporter.report(node);
+                return false;
+            }
+
+            node.addAttribute(Attribute.TYPES, Arrays.asList(firstChild.getAttribute(Attribute.TYPE)));
+            return true;
+        }
+
+        Node thirdChild = node.getChild(0);
+        String thirdSymbol = thirdChild.name();
+        // <lista_argumenata> ::= <lista_argumenata> ZAREZ <izraz_pridruzivanja>
+        if ("<lista_argumenata>".equals(firstSymbol)) {
+
+            // 1. provjeri(<lista_argumenata>)
+            if (!firstChild.check()) {
+                SemanticErrorReporter.report(node);
+                return false;
+            }
+
+            // 2. provjeri(<izraz_pridruzivanja>)
+            if (!thirdChild.check()) {
+                SemanticErrorReporter.report(node);
+                return false;
+            }
+
+            List<VariableType> types = new ArrayList<>((List<VariableType>) firstChild.getAttribute(Attribute.TYPES));
+            types.add((VariableType) thirdChild.getAttribute(Attribute.TYPE));
+            node.addAttribute(Attribute.TYPES, types);
+            return true;
+        }
+
+        System.err.println("Shold never happen");
+        SemanticErrorReporter.report(node);
         return false;
     }
 
--- semantic-analysis/src/main/java/hr/fer/zemris/ppj/semantic/rule/expressions/UnaryExpressionChecker.java
+++ semantic-analysis/src/main/java/hr/fer/zemris/ppj/semantic/rule/expressions/UnaryExpressionChecker.java
@@ -1,6 +1,9 @@
 package hr.fer.zemris.ppj.semantic.rule.expressions;
 
+import hr.fer.zemris.ppj.Attribute;
 import hr.fer.zemris.ppj.Node;
+import hr.fer.zemris.ppj.SemanticErrorReporter;
+import hr.fer.zemris.ppj.VariableType;
 import hr.fer.zemris.ppj.semantic.rule.Checker;
 
 /**
@@ -36,7 +39,101 @@ public class UnaryExpressionChecker implements Checker {
      */
     @Override
     public boolean check(Node node) {
-        // TODO Auto-generated method stub
+        Node firstChild = node.getChild(0);
+        String firstSymbol = firstChild.name();
+
+        // <unarni_izraz> ::= <postfiks_izraz>
+        if ("<postfiks_izraz>".equals(firstSymbol)) {
+
+            // 1. provjeri(<posfiks_izraz>)
+            if (!firstChild.check()) {
+                SemanticErrorReporter.report(node);
+                return false;
+            }
+
+            node.addAttribute(Attribute.TYPE, firstChild.getAttribute(Attribute.TYPE));
+            node.addAttribute(Attribute.L_EXPRESSION, firstChild.getAttribute(Attribute.L_EXPRESSION));
+            return true;
+        }
+
+        Node secondChild = node.getChild(1);
+        String secondSymbol = secondChild.name();
+
+        // <unarni_izraz> ::= OP_INC <unarni_izraz>
+        if ("OP_INC".equals(firstSymbol)) {
+
+            // 1. provjeri(<unarni_izraz>)
+            if (!secondChild.check()) {
+                SemanticErrorReporter.report(node);
+                return false;
+            }
+
+            // 2. <unarni_izraz>.l-izraz = 1
+            if (!secondChild.getAttribute(Attribute.L_EXPRESSION).equals(true)) {
+                SemanticErrorReporter.report(node);
+                return false;
+            }
+
+            // 2. <unarni_izraz.tip ~ int
+            if (!VariableType.implicitConversion((VariableType) secondChild.getAttribute(Attribute.TYPE),
+                    VariableType.INT)) {
+                SemanticErrorReporter.report(node);
+                return false;
+            }
+
+            node.addAttribute(Attribute.TYPE, VariableType.INT);
+            node.addAttribute(Attribute.L_EXPRESSION, false);
+            return true;
+        }
+        // <unarni_izraz> ::= OP_DEC <unarni_izraz>
+        if ("OP_DEC".equals(firstSymbol)) {
+
+            // 1. provjeri(<unarni_izraz>)
+            if (!secondChild.check()) {
+                SemanticErrorReporter.report(node);
+                return false;
+            }
+
+            // 2. <unarni_izraz>.l-izraz = 1
+            if (!secondChild.getAttribute(Attribute.L_EXPRESSION).equals(true)) {
+                SemanticErrorReporter.report(node);
+                return false;
+            }
+
+            // 2. <unarni_izraz.tip ~ int
+            if (!VariableType.implicitConversion((VariableType) secondChild.getAttribute(Attribute.TYPE),
+                    VariableType.INT)) {
+                SemanticErrorReporter.report(node);
+                return false;
+            }
+
+            node.addAttribute(Attribute.TYPE, VariableType.INT);
+            node.addAttribute(Attribute.L_EXPRESSION, false);
+            return true;
+        }
+
+        // <unarni_izraz> ::= <unarni_operator> <cast_izraz>
+        if ("<unarni_operator>".equals(firstSymbol)) {
+
+            // 1. provjeri(<cast_izraz>)
+            if (!secondChild.check()) {
+                SemanticErrorReporter.report(node);
+                return false;
+            }
+
+            // 2. <cast_izraz>.tip ~ int
+            if (!VariableType.implicitConversion((VariableType) secondChild.getAttribute(Attribute.TYPE),
+                    VariableType.INT)) {
+                SemanticErrorReporter.report(node);
+                return false;
+            }
+
+            node.addAttribute(Attribute.TYPE, VariableType.INT);
+            node.addAttribute(Attribute.L_EXPRESSION, false);
+            return true;
+        }
+        System.err.println("Shold never happen");
+        SemanticErrorReporter.report(node);
         return false;
     }
 
