aa81d5bd5ec6b9dd2eb991c0ba5aa59db5a2d57e
Filip Gulan <filip.gulan@infinum.hr>
2016-01-20 09:24:34 +0100

    Jel sad?

--- code-generation/src/main/java/hr/fer/zemris/ppj/BinaryOperation.java
+++ code-generation/src/main/java/hr/fer/zemris/ppj/BinaryOperation.java
@@ -1,5 +1,5 @@
 package hr.fer.zemris.ppj;
 
 public enum BinaryOperation {
-    ADD, SUB, MUL, DIV, OR, AND, XOR, EQ, NE, LT, GT, LE, GE
+    ADD, SUB, MUL, DIV, OR, AND, XOR, EQ, NE, LT, GT,  LE, GE
 }
